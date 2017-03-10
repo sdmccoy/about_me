4519e7556f59440170224777e413c4a2931cddd3
Scott McCoy <Scott McCoy>
2017-03-10 01:53:43 -0800

    Changed all of the questions into one function. Created var arrays for the questions and responses. Created a score systems because not all Yes answers are correct. Fixed the question 6. Fixed the final count being stuck in the function 7. All is tested and functioning.

--- app.js
+++ app.js
@@ -3,107 +3,33 @@
 var counter = 0;
 var questionName = 'What is your name?';
 var userName = prompt(questionName);
+console.log(questionName + ': ' + userName);
 
-function question(){
-  console.log(questionName + ': ' + userName);
-  alert('Howdy ' + userName + '!');
-};
-question();
+var questionPrompt = ['Do you want to play a game of memory?', 'Is my last name MacCoy?', 'Did I attend two colleges?', 'Did I mention I\'m back in school again?', 'Is my nickname Dingo?'];
+var responseYes = ['Superb! Hopefully you read my bio!', 'Incorrect, it\'s McCoy, attention to detail is important!', 'Bingo! Technically 3 but you didn\'t know that.', 'Correct! You must have gone to school for being so smart.', 'Correct! You must know voodoo because I never mentioned that.'];
+var responseNo = ['Sad panda, come back later when you\'re not scared.', 'Who ever thought answering \'no\' could feel so right.', 'Wrong; maybe alzheimers is setting in.', 'Wah wah; I would see a dr. for your memory.', 'Wrong but I won\'t fault you because I never mentioned it.'];
+var userScore = [[1,0],[0,1],[1,0],[1,0],[1,0]];
 
 function question1(){
-  var question1 = 'Do you want to play a game of memory?';
-  var answer1 = prompt(question1);
-  if (answer1.toLowerCase() === 'y' || answer1.toLowerCase() === 'yes') {
-    var responseYes1 = alert('Superb! Hopefully you read my bio!');
-    console.log(question1 + ': ' + answer1);
-    counter++;
-  }
-  else if (answer1.toLowerCase() === 'n' || answer1.toLowerCase() === 'no') {
-    var responseNo1 = alert('Sad panda, come back later when you\'re not scared.');
-    console.log(question1 + ': ' + answer1);
-  }
-  else {
-    var responseNA1 = alert('Woah there, tiger; try using english.');
-    console.log(question1 + ': ' + answer1);
+  for (var i = 0; i < questionPrompt.length; i++) {
+    var answer = prompt(questionPrompt[i]);
+    if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes') {
+      alert(responseYes[i]);
+      console.log(questionPrompt[i] + ': ' + answer);
+      counter += userScore[i][0];
+    }
+    else if (answer.toLowerCase() === 'n' || answer.toLowerCase() === 'no') {
+      alert(responseNo[i]);
+      console.log(questionPrompt[i] + ': ' + answer);
+      counter += userScore[i][1];
+    }
+    else {
+      alert('Woah there, tiger; try using english.');
+      console.log(questionPrompt[i] + ': ' + answer);
+    }
   }
 }
 question1();
-
-function question2(){
-  var question2 = '  Is my last name MacCoy?';
-  var answer2 = prompt(question2);
-  if (answer2.toLowerCase() === 'y' || answer2.toLowerCase() === 'yes') {
-    var responseYes2 = alert('Incorrect, it\'s McCoy, attention to detail is important!');
-    console.log(question2 + ': ' + answer2);
-  }
-  else if (answer2.toLowerCase() === 'n' || answer2.toLowerCase() === 'no') {
-    var responseNo2 = alert('Who ever thought answering \'no\' could feel so right.');
-    console.log(question2 + ': ' + answer2);
-    counter++;
-  }
-  else {
-    var responseNA2 = alert('Woah there, tiger; try using english.');
-    console.log(question2 + ': ' + answer2);
-  }
-}
-question2();
-
-function question3(){
-  var question3 = 'Did I attend two colleges?';
-  var answer3 = prompt(question3);
-  if (answer3.toLowerCase() === 'y' || answer3.toLowerCase() === 'yes') {
-    var responseYes3 = alert('Bingo! Technically 3 but you didn\'t know that.');
-    console.log(question3 + ': ' + answer3);
-    counter++;
-  }
-  else if (answer3.toLowerCase() === 'n' || answer3.toLowerCase() === 'no') {
-    var responseNo3 = alert('Wrong; maybe alzheimers is setting in.');
-    console.log(question3 + ': ' + answer3);
-  }
-  else {
-    var responseNA3 = alert('Woah there, tiger; try using english.');
-    console.log(question3 + ': ' + answer3);
-  }
-}
-question3();
-
-function question4(){
-  var question4 = 'Did I mention I\'m back in school again?';
-  var answer4 = prompt(question4);
-  if (answer4.toLowerCase() === 'y' || answer4.toLowerCase() === 'yes') {
-    console.log(question4 + ': ' + answer4);
-    var responseYes4 = alert('Correct! You must have gone to school for being so smart.');
-    console.log(question4 + ': ' + answer4);
-    counter++;
-  }
-  else if (answer4.toLowerCase() === 'n' || answer4.toLowerCase() === 'no') {
-    var responseNo4 = alert('Wah wah; I would see a dr. for your memory.');
-    console.log(question4 + ': ' + answer4);
-  }
-  else {
-    var responseNA4 = alert('Woah there, tiger; try using english.');
-    console.log(question4 + ': ' + answer4);
-  }
-}
-question4();
-
-function question5(){
-  var question5 = 'Is my nickname Dingo?';
-  var answer5 = prompt(question5);
-  if (answer5.toLowerCase() === 'y' || answer5.toLowerCase() === 'yes') {
-    var responseYes5 = alert('Correct! You must know voodoo because I never mentioned that.');
-    console.log(question5 + ': ' + answer5);
-    counter++;
-  }
-  else if (answer5.toLowerCase() === 'n' || answer5.toLowerCase() === 'no') {
-    var responseNo5 = alert('Wrong but I won\'t fault you because I never mentioned it.');
-    console.log(question5 + ': ' + answer5);
-  }
-  else {
-    var responseNA5 = alert('Woah there, tiger; try using english.');
-  }
-}
-question5();
 
 function question6(){
   var attempts1 = 1;
@@ -118,7 +44,7 @@ function question6(){
       alert('Too low. I\'m not that lazy.');
       console.log(question6 + ': ' + answer6);
       attempts1++;
-    } else {
+    } else if (parseInt(answer6) === 10) {
       alert('Bingo! You\'re a ninja.');
       console.log(question6 + ': ' + answer6);
       counter++;
@@ -144,12 +70,13 @@ function question7(){
     }
   }
   alert('Here\'s all of the possible answers: ' + newArray1);
-  if (counter > 5) {
-    alert(counter + ' out of 7! ' + userName + ' is a smart cookie!');
-  } else if (counter < 3) {
-    alert('Ouch, only ' + counter + ' out of 7. ' + userName + ' , go back to school.');
-  } else {
-    alert(counter + ' out of 7. Mediocre at best, ' + userName + '.');
-  }
 }
 question7();
+
+if (counter > 5) {
+  alert(counter + ' out of 7! ' + userName + ' is a smart cookie!');
+} else if (counter < 3) {
+  alert('Ouch, only ' + counter + ' out of 7. ' + userName + ' , go back to school.');
+} else {
+  alert(counter + ' out of 7. Mediocre at best, ' + userName + '.');
+}
